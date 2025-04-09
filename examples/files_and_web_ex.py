5579a8704f9e4792ed601aba970cb74fe83b8032
Redflymeer <redflymeer@gmail.com>
2025-04-09 17:16:29 +0300

    Update files_and_web_ex.py

--- examples/files_and_web_ex.py
+++ examples/files_and_web_ex.py
@@ -1,6 +1,6 @@
-from easy_python.web import fetch_url, scrape_links
-from easy_python.files import write_json
+from EasyPyTools.web import fetch_url, scrape_links
+from EasyPyTools.files import write_json
 
-# Получить ссылки с сайта
+# Getting url
 links = scrape_links("https://python.org")
 write_json({"links": links}, "python_links.json")
